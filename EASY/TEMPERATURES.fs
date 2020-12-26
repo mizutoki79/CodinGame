6953bedc1a68b6c24d302d3fb0cf97558baee415
mizutoki_ <a.p9xelic5@gmail.com>
2020-12-26 18:20:33 +0900

    F#のEASY TEMPERTURES を追加

---
+++ EASY/TEMPERATURES.fs
@@ -0,0 +1,5 @@
+﻿ignore (stdin.ReadLine())
+(stdin.ReadLine()).Split ' ' |> Array.filter ((<>)"") |> function
+| a when (Array.length a) > 0 -> a |> Array.map int |> Seq.sortBy (fun x -> abs x, -x) |> Seq.head
+|_ -> 0
+|> stdout.WriteLine
