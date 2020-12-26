da8ac92b913fc3b4859512da000ccea3cc06c2fc
mizutoki_ <a.p9xelic5@gmail.com>
2020-12-27 01:22:21 +0900

    power of thor episode 1 を改善

--- EASY/PowerofThor-Episode1.fsx
+++ EASY/PowerofThor-Episode1.fsx
@@ -2,31 +2,16 @@ open System
 open System.Text
 #nowarn "25"
 
+let rec gameLoop lightX lightY thorX thorY =
+    stdin.ReadLine() |> ignore
+    let dy = compare lightY thorY
+    let dx = compare lightX thorX
+    printfn "%s" (["N"; ""; "S"].[dy + 1] + ["W"; ""; "E"].[dx + 1])
+    gameLoop lightX lightY (thorX + dx) (thorY + dy)
+
 let (lightX, lightY, initialTX, initialTY) =
     stdin.ReadLine().Split ' ' |> Array.map int |> function
     | [| a; b; c; d |] -> (a, b, c, d)
     |_ -> failwith "invalid input"
 
-let mutable (thorX, thorY) = (initialTX, initialTY)
-
-let rec gameLoop () =
-    eprintfn "lignt %A" (lightX, lightY)
-    eprintfn "thor %A" (thorX, thorY)
-    stdin.ReadLine() |> ignore
-    let mutable direction = String.Empty
-    if lightY > thorY then
-        direction <- "N"
-        thorY <- thorY - 1
-    elif lightY < thorY then
-        direction <- "S"
-        thorY <- thorY + 1
-    if lightX > thorX then
-        direction <- direction + "E"
-        thorX <- thorX + 1
-    elif lightX < thorX then
-        direction <- direction + "W"
-        thorX <- thorX - 1
-    printfn "%s" direction
-    gameLoop()
-
-gameLoop() |> ignore
+gameLoop lightX lightY initialTX initialTY |> ignore
