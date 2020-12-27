1c014e455be00a200c80082db99f80feadec2e2a
mizutoki_ <a.p9xelic5@gmail.com>
2020-12-27 18:23:09 +0900

    F# TicTacToe を改善

--- EASY/TICTACTOE.fsx
+++ EASY/TICTACTOE.fsx
@@ -1,72 +1,18 @@
-open System
-
 let dim = 3
+let tiles = Seq.init (dim * dim) id
+let combinations = Seq.concat [
+    yield tiles |> Seq.groupBy (fun i -> i / dim) |> Seq.map snd
+    yield tiles |> Seq.groupBy (fun i -> i % dim) |> Seq.map snd
+    yield seq {seq {for i in 0 .. dim - 1 do yield dim * i  + i}; seq {for i in 1 .. dim do yield dim * i - i}}
+]
 
-let board = [|
-    for i = 0 to dim - 1 do
-        yield stdin.ReadLine() |> Seq.toArray
-|]
+let board = [| yield! Seq.init dim (fun _ -> stdin.ReadLine() |> Seq.toArray) |] |> Array.concat
 
-let canWin line =
-    let length = line |> Array.length
-    let circleCount = line |> Seq.filter ((=)'O') |> Seq.length
-    let emptyCount = line |> Seq.filter ((=)'.') |> Seq.length
-    circleCount = length - 1 && emptyCount = 1
-
-let rec resolve direction i =
-    match direction with
-        | 'h' ->
-            let line = board.[i]
-            if canWin line then
-                board.[i] <- [|'O'; 'O'; 'O'|]
-                true
-            elif i < dim - 1 then
-                resolve 'h' (i + 1)
-            else
-                resolve 'v' 0
-        | 'v' ->
-            let line = [|board.[0].[i]; board.[1].[i]; board.[2].[i]|]
-            if canWin line then
-                for j = 0 to dim - 1 do
-                    board.[j].[i] <- 'O'
-                true
-            elif i < dim - 1 then
-                resolve 'v' (i + 1)
-            else
-                resolve 'o' 0
-        | 'o' ->
-            let line = i |> function
-                | 0 ->
-                    [|
-                        for j = 0 to dim - 1 do
-                            yield board.[j].[j]
-                    |]
-                | 1 ->
-                    [|
-                        for j = 0 to dim - 1 do
-                            yield board.[j].[dim - 1 - j]
-                    |]
-                |_ -> Array.create dim 'X'
-            if canWin line then
-                i |> function
-                    | 0 ->
-                        for j = 0 to dim - 1 do
-                            board.[j].[j] <- 'O'
-                        true
-                    | 1 ->
-                        for j = 0 to dim - 1 do
-                            board.[j].[dim - 1 - j] <- 'O'
-                        true
-                    |_ -> false
-            elif i = 0 then
-                resolve 'o' 1
-            else
-                false
-        |_ -> false
-
-
-if resolve 'h' 0 then
-    for i = 0 to dim - 1 do
-        printfn "%s" (String board.[i])
-else
-    printfn "false"
+combinations
+    |> Seq.tryFind (fun c -> c |> Seq.sumBy (fun i -> if board.[i] = 'O' then 1 elif board.[i] = 'X' then -1 else 0) = dim - 1)
+    |> function
+        | Some c ->
+            let move = c |> Seq.find (fun i -> board.[i] = '.')
+            board.[move] <- 'O'
+            for i = 0 to dim - 1 do printfn "%s" (board |> Seq.skip (i * dim) |> Seq.take dim |> Seq.toArray |> System.String)
+        | None -> printfn "false"
